f1528342a180283ee5f9e177548adee422fa65a3
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2025-01-18 13:39:23 +0545

    changes

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,38 +1,40 @@
-import { Response ,Request, response} from "express";
+import { Request, Response } from "express";
 import Order from "../database/Models/orderModel";
-import { PaymentMethod } from "../globals/types";
-import Payment from "../database/Models/paymentModel";
 import OrderDetails from "../database/Models/orderDetails";
-import axios from 'axios';
+import { PaymentMethod, PaymentStatus } from "../globals/types";
+import Payment from "../database/Models/paymentModel";
+import axios from 'axios'
+
 interface IProduct{
-  productId : string, 
-  productQty : string 
+    productId : string, 
+    productQty : string 
 }
 
 interface OrderRequest extends Request{
-    user? :{
-        id:string
+    user? : {
+        id : string
     }
 }
 
 class OrderController{
-    async createOrder(req:OrderRequest,res:Response){
-      const userId=  req.user?.id;
-        const{phoneNumber,shippingAddress,totalAmount,paymentMethod}=req.body
+    static async createOrder(req:OrderRequest,res:Response):Promise<void>{
+        const userId =  req.user?.id
+        const {phoneNumber,shippingAddress,totalAmount,paymentMethod} = req.body 
         const products:IProduct[] = req.body.products
-        if(!phoneNumber||!shippingAddress||!totalAmount||products.length==0 ){
-res.status(400).json({
-    message:"Please fill phonenumber,shippingaddress,total amount,products"
-})
-return
+        if(!phoneNumber || !shippingAddress || !totalAmount || products.length == 0 ){
+            res.status(400).json({
+                message : "Please provide phoneNumber,shippingAddress,totalAmount,products"
+            })
+            return
         }
-      const orderData =  await Order.create({
-        phoneNumber,
-        shippingAddress,
-        totalAmount,
-        userId
-
-      })
+        // for order 
+        const orderData = await Order.create({
+            phoneNumber, 
+            shippingAddress, 
+            totalAmount, 
+            userId
+        })
+        // for orderDetails
       products.forEach(async function(product){
         await OrderDetails.create({
             quantity : product.productQty, 
@@ -40,38 +42,75 @@ return
             orderId : orderData.id
         })
       })
-      // for payment 
-      const paymentData= await Payment.create({
+      // for payment
+      const paymentData = await Payment.create({
         orderId : orderData.id, 
         paymentMethod : paymentMethod, 
     })
-      if(paymentMethod == PaymentMethod.COD){
-        const data={
-          return_url:"https://localhost:5173/",
-          website_url:"https://localhost:5173/",
-          amount:totalAmount*100,  //rupees to paisa
-          purchase_order_id:orderData.id,
-          purchase_order_name:"order_"+orderData.id
+     if (paymentMethod == PaymentMethod.Khalti){
+        // khalti logic
+        
+        const data = {
+          return_url : "http://localhost:5173/", 
+          website_url : "http://localhost:5173/", 
+          amount : totalAmount * 100, 
+          purchase_order_id : orderData.id, 
+          purchase_order_name : "order_" + orderData.id
         }
-        const response =  await axios.post("https://a.khalti.com/api/v2/epayment/initiate",data,{
-        headers:{
-          authorization:"key a35315b63d7f48318e0170cfd18ffb78"
-        }
-       })
-       const khaltiResponse = response.data 
-       paymentData.pidx = khaltiResponse.pidx
-       paymentData.save()
-       res.status(200).json({
-         message : "Order created successfully", 
-         url : khaltiResponse.payment_url
-       })
-
+       const response =  await axios.post("https://a.khalti.com/api/v2/epayment/initiate/",data,{
+          headers : {
+            "Authorization" : "Key a35315b63d7f48318e0170cfd18ffb78"
+          }
+        })
+      const khaltiResponse = response.data 
+      paymentData.pidx = khaltiResponse.pidx
+      paymentData.save()
+      res.status(200).json({
+        message : "Order created successfully", 
+        url : khaltiResponse.payment_url, 
+        pidx : khaltiResponse.pidx
+      })
+      }else if(paymentMethod == PaymentMethod.Esewa){
 
       }else{
-        // esewa logic
+        res.status(200).json({
+          message : "Order created successfully"
+        })
       }
-     
+ 
+    }
+    static async verifyTransaction(req:OrderRequest,res:Response):Promise<void>{
+      const {pidx} = req.body 
+      if(!pidx){
+        res.status(400).json({
+          message : "Please provide pidx"
+        })
+        return
+      }
+      const response = await axios.post("https://a.khalti.com/api/v2/epayment/lookup/",{
+        pidx : pidx
+      },{
+        headers : {
+          "Authorization" : "Key a35315b63d7f48318e0170cfd18ffb78"
+        }
+      })
+      const data = response.data 
+      if(data.status === "Completed"){
+        await Payment.update({paymentStatus : PaymentStatus.Paid},{
+          where : {
+            pidx : pidx 
+          }
+        })
+        res.status(200).json({
+          message : "Payment verified successfully !!"
+        })
+      }else{
+        res.status(200).json({
+          message : "Payment not verified or cancelled"
+        })
+      }
+
     }
 }
 
-export default new OrderController()
+export default OrderController
--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import orderController from "../controllers/orderController";
 import userMiddleware from "../middleware/userMiddleware";
+import errorHandler from "../services/errorHandler";
 const router: Router = express.Router();
 router.post("/", userMiddleware.isUserLoggedIn, orderController.createOrder);
+router.route("/verify-pidx").post(userMiddleware.isUserLoggedIn,errorHandler(orderController.verifyTransaction))
 export default router;
